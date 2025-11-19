fc4a07884fd00728d5e1c474b2bc13c5e87eb1dd
LamGC <lam827@lamgc.net>
2025-11-19 18:51:33 +0800

    feat: Add support for dynamic config updates (Node.js)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import {getConfig} from "@/lib/config";
 import {HomePage} from "@/components/pages/HomePage";
 
-export const runtime = 'nodejs';
+/*
+ * When the runtime is "edge", please remove revalidate.
+ * When the runtime is "nodejs", please set revalidate to 30~90 sec.
+ */
+export const runtime = 'edge';
+// export const revalidate = 60;
 
 export default async function Home() {
     const config = await getConfig();
