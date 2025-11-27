866eb1954d7b426008c7e1605c9d84bf4e739f61
Yorlg <1839033430@qq.com>
2025-11-27 07:57:40 +0000

    build: 移除废弃的@cloudflare/next-on-pages 改用@opennextjs/cloudflare 解决.
    Issue #14

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
-import {getConfig} from "@/lib/config";
-import {HomePage} from "@/components/pages/HomePage";
+import { getConfig } from '@/lib/config';
+import { HomePage } from '@/components/pages/HomePage';
 
-/*
- * When the runtime is "edge", please remove revalidate.
- * When the runtime is "nodejs", please set revalidate to 30~90 sec.
- */
-export const runtime = 'edge';
 // export const revalidate = 60;
 
 export default async function Home() {
-    const config = await getConfig();
+  const config = await getConfig();
 
-    return (
-      <HomePage
-        profile={config.profile}
-        socialLinks={config.socialLinks}
-        websiteLinks={config.websiteLinks}
-      />
-    );
+  return (
+    <HomePage
+      profile={config.profile}
+      socialLinks={config.socialLinks}
+      websiteLinks={config.websiteLinks}
+    />
+  );
 }
