ce1891e9166badf90bba1ca833618d702be815bf
Yorlg <1839033430@qq.com>
2026-01-30 01:39:47 +0000

    "fix: remove Cloudflare config for Netlify deployment"

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from 'next';
 
-/**
- * @type {import('next').NextConfig}
- */
 const nextConfig: NextConfig = {
   /* config options here */
   images: { unoptimized: true },
@@ -20,11 +16,5 @@ const nextConfig: NextConfig = {
     ];
   },
 };
-
-if (process.env.NODE_ENV === 'development') {
-  (async () => {
-    await setupDevPlatform();
-  })();
-}
 
 export default nextConfig;
